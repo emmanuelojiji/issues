e0926f64ee0fd779cb81c9071e9bef59ea588123
emmanuelojiji <emmanuelojiji@hotmail.com>
2023-08-09 09:21:10 +0100

    Complete to do click added

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import "./ToDoItem.scss"
-const ToDoItem = ({text}) => {
-    return (
-        <div className='to-do-item'>
-          <p>{text}</p>  
-          <div className='to-do-item-circle'></div>
-        </div>
-    );
+import React, { useState } from "react";
+import "./ToDoItem.scss";
+
+const ToDoItem = ({ text }) => {
+  const [complete, setComplete] = useState(false);
+  return (
+    <div className="to-do-item">
+      <p>{text}</p>
+      <div
+        className={`to-do-item-circle ${complete ? "complete" : null}`}
+        onClick={() => setComplete(!complete)}
+      ></div>
+    </div>
+  );
 };
 
 export default ToDoItem;
