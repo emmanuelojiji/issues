41967753c04959789b0d794c32aa612d55a215e7
emmanuelojiji <emmanuelojiji@hotmail.com>
2023-08-09 09:58:41 +0100

    Opacity reduced on completed to do

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -3,8 +3,10 @@ import "./ToDoItem.scss";
 
 const ToDoItem = ({ text }) => {
   const [complete, setComplete] = useState(false);
+
+  const [amount, setAmount] = useState(3)
   return (
-    <div className="to-do-item">
+    <div className={`to-do-item ${complete ? "fade-out" : null}`}>
       <p>{text}</p>
       <div
         className={`to-do-item-circle ${complete ? "complete" : null}`}
